9b425108472ce7094a9ecaf137e189b94177b0e7
Ish Kapoor <60035881+ishkapoor2000@users.noreply.github.com>
2020-12-05 00:38:57 +0530

    Update Custom_ML_Tools.py

--- Custom_ML_Tools.py
+++ Custom_ML_Tools.py
@@ -14,7 +14,7 @@ if st.checkbox("Upload Dataset"):
     if csv_file is not None:
         st.write(type(csv_file))
         df = pd.read_csv(csv_file)
-    
+
         if st.checkbox("Preview Dataset"):
             data = df
             if st.button("HEAD"):
@@ -23,10 +23,10 @@ if st.checkbox("Upload Dataset"):
                 st.write(data.tail(50))
             else:
                 st.write(data)
-        
+
         if st.checkbox("View Columns"):
             st.write(df.columns)
-        
+
         data_dmns = st.radio("", ("Rows", "Columns", "All"))
         if data_dmns == "Rows":
             st.text("Showing Rows")
@@ -37,10 +37,10 @@ if st.checkbox("Upload Dataset"):
         else:
             st.text("Showing Shape")
             st.write(df.shape)
-        
+
         describe = st.write(df.describe())
         describe
-        
+
         columns = [c for c in df.columns]
         colm = st.selectbox("Select Column", tuple(columns))
         for i in range(len(columns)):
@@ -48,6 +48,6 @@ if st.checkbox("Upload Dataset"):
                 st.write(df[columns[i]])
                 chart_data = pd.DataFrame(df[:40], columns = [columns[i]])
                 st.area_chart(chart_data)
-        
+
         barchart = st.bar_chart(df[columns])
         linechart = st.line_chart(df)
