2371fc6d3b6b1736595decbaf8f8e17f02b19e0b
Ish Kapoor <60035881+ishkapoor2000@users.noreply.github.com>
2020-12-05 21:07:18 +0530

    Update Custom_ML_Tools.py

--- Custom_ML_Tools.py
+++ Custom_ML_Tools.py
@@ -62,7 +62,43 @@ elif choice == "Upload Dataset":
 
             barchart = st.bar_chart(df[columns])
             linechart = st.line_chart(df)
+        else:
+		if st.checkbox("Preview Dataset"):
+                data = df
+                if st.button("HEAD"):
+                    st.write(data.head(50))
+                elif st.button("TAIL"):
+                    st.write(data.tail(50))
+                else:
+                    st.write(data)
 
+            if st.checkbox("View Columns"):
+                st.write(df.columns)
+
+            data_dmns = st.radio("", ("Rows", "Columns", "All"))
+            if data_dmns == "Rows":
+                st.text("Showing Rows")
+                st.write(df.shape[0])
+            elif data_dmns == "Columns":
+                st.text("Showing Columns")
+                st.write(df.shape[1])
+            else:
+                st.text("Showing Shape")
+                st.write(df.shape)
+
+            describe = st.write(df.describe())
+            describe
+
+            columns = [c for c in df.columns]
+            colm = st.selectbox("Select Column", tuple(columns))
+            for i in range(len(columns)):
+                if colm == columns[i]:
+                    st.write(df[columns[i]])
+                    chart_data = pd.DataFrame(df[:40], columns=[columns[i]])
+                    st.area_chart(chart_data)
+
+            barchart = st.bar_chart(df[columns])
+            linechart = st.line_chart(df)
 
 else:
     st.write("***\
