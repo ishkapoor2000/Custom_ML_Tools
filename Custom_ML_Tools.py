9270249ebb175fef592391aa7791b223fcbbbfd9
Ish Kapoor <60035881+ishkapoor2000@users.noreply.github.com>
2020-12-05 20:50:41 +0530

    Update Custom_ML_Tools.py

--- Custom_ML_Tools.py
+++ Custom_ML_Tools.py
@@ -8,46 +8,64 @@ import streamlit as st
 
 st.title("Preview Dataset")
 
-df = None
-if st.checkbox("Upload Dataset"):
-    csv_file = st.file_uploader("Upload '.CSV File'", type=["csv"])
-    if csv_file is not None:
-        st.write(type(csv_file))
-        df = pd.read_csv(csv_file)
+menu = ["Home", "Upload Dataset", "About"]
+choice = st.sidebar.selectbox("Menu:", menu)
 
-        if st.checkbox("Preview Dataset"):
-            data = df
-            if st.button("HEAD"):
-                st.write(data.head(50))
-            elif st.button("TAIL"):
-                st.write(data.tail(50))
+if choice == "Home":
+        st.write("***\
+                 Created by Ish Kapoor\
+                 ***")
+
+elif choice == "Upload Dataset":
+    df = None
+    st.subheader("Upload Dataset")
+    if st.checkbox("Upload Dataset"):
+        csv_file = st.file_uploader("Upload '.CSV File'/'.xlsx File'", type=["csv", "xlsx"])
+        if csv_file is not None:
+            st.write(type(csv_file))
+            df = pd.read_csv(csv_file)
+
+            if st.checkbox("Preview Dataset"):
+                data = df
+                if st.button("HEAD"):
+                    st.write(data.head(50))
+                elif st.button("TAIL"):
+                    st.write(data.tail(50))
+                else:
+                    st.write(data)
+
+            if st.checkbox("View Columns"):
+                st.write(df.columns)
+
+            data_dmns = st.radio("", ("Rows", "Columns", "All"))
+            if data_dmns == "Rows":
+                st.text("Showing Rows")
+                st.write(df.shape[0])
+            elif data_dmns == "Columns":
+                st.text("Showing Columns")
+                st.write(df.shape[1])
             else:
-                st.write(data)
+                st.text("Showing Shape")
+                st.write(df.shape)
 
-        if st.checkbox("View Columns"):
-            st.write(df.columns)
+            describe = st.write(df.describe())
+            describe
 
-        data_dmns = st.radio("", ("Rows", "Columns", "All"))
-        if data_dmns == "Rows":
-            st.text("Showing Rows")
-            st.write(df.shape[0])
-        elif data_dmns == "Columns":
-            st.text("Showing Columns")
-            st.write(df.shape[1])
-        else:
-            st.text("Showing Shape")
-            st.write(df.shape)
+            columns = [c for c in df.columns]
+            colm = st.selectbox("Select Column", tuple(columns))
+            for i in range(len(columns)):
+                if colm == columns[i]:
+                    st.write(df[columns[i]])
+                    chart_data = pd.DataFrame(df[:40], columns=[columns[i]])
+                    st.area_chart(chart_data)
 
-        describe = st.write(df.describe())
-        describe
+            barchart = st.bar_chart(df[columns])
+            linechart = st.line_chart(df)
 
-        columns = [c for c in df.columns]
-        colm = st.selectbox("Select Column", tuple(columns))
-        for i in range(len(columns)):
-            if colm == columns[i]:
-                st.write(df[columns[i]])
-                chart_data = pd.DataFrame(df[:40], columns=[columns[i]])
-                st.area_chart(chart_data)
 
-        barchart = st.bar_chart(df[columns])
-        linechart = st.line_chart(df)
+else:
+    st.write("***\
+             Creator: ISH KAPOOR\
+             ***)
+
+    st.write("View GitHub Repo: 'https://github.com/ishkapoor2000/Custom_ML_Tools'")
